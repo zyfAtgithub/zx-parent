8fd1b84ca707ce43c8be6c92a1476de8accac369
zhangyifeng <18226921038@163.com>
2017-11-02 10:39:42 +0800

    send email.

--- zx-parent/zx-core/src/main/java/com/yf/zx/core/util/email/SendMail.java
+++ zx-parent/zx-core/src/main/java/com/yf/zx/core/util/email/SendMail.java
@@ -1,56 +1,72 @@
 package com.yf.zx.core.util.email;
 
+// 文件名 SendEmail.java
 import java.util.Properties;
 
+import javax.mail.Authenticator;
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 public class SendMail {
-    public static void main(String[] args) throws Exception {
-        
-        Properties prop = new Properties();
-        prop.setProperty("mail.host", "smtp.163.com");
-        prop.setProperty("mail.transport.protocol", "smtp");
-        prop.setProperty("mail.smtp.auth", "true");
-        //使用JavaMail发送邮件的5个步骤
-        //1、创建session
-        Session session = Session.getInstance(prop);
-        //开启Session的debug模式，这样就可以查看到程序发送Email的运行状态
-        session.setDebug(true);
-        //2、通过session得到transport对象
-        Transport ts = session.getTransport();
-        //3、使用邮箱的用户名和密码连上邮件服务器，发送邮件时，发件人需要提交邮箱的用户名和密码给smtp服务器，用户名和密码都通过验证之后才能够正常发送邮件给收件人。
-        ts.connect("smtp.163.com", "ismalert@163.com", "1q2w3e4r");
-        //4、创建邮件
-        Message message = createSimpleMail(session);
-        //5、发送邮件
-        ts.sendMessage(message, message.getAllRecipients());
-        ts.close();
-    }
-    
-    /**
-    * createSimpleMail  创建一封只包含文本的邮件
-    * 孤傲苍狼
-    * @param session 邮件会话
-    * @return MimeMessage 邮件
-    * @throws Exception 异常
-    */ 
-    public static MimeMessage createSimpleMail(Session session)
-            throws Exception {
-        //创建邮件对象
-        MimeMessage message = new MimeMessage(session);
-        //指明邮件的发件人
-        message.setFrom(new InternetAddress("ismalert@163.com"));
-        //指明邮件的收件人，现在发件人和收件人是一样的，那就是自己给自己发
-        message.setRecipient(Message.RecipientType.TO, new InternetAddress("1534708324@qq.com"));
-        //邮件的标题
-        message.setSubject("xsx");
-        //邮件的文本内容
-        message.setContent("xasxa", "text/html;charset=UTF-8");
-        //返回创建好的邮件对象
-        return message;
-    }
+	
+	/** 发件人电子邮箱 */
+	private static final String MAIL_SENDER = "system_hdp@ctmcdn.cn";
+
+	/** 发件人密码 */
+	private static final String MAIL_SENDER_PWD = "04e9cd91354c3c4d";
+
+	/** 发件人密码 */
+	private static final String MAIL_SMTP_AUTH = "true";
+	
+	/** 邮件服务器 */
+	private static final String MAIL_SMTP_HOST = "mail.ctmcdn.cn";
+	
+	/** 收件人电子邮箱*/
+	private static final String MAIL_RECEIVER = "15295778261@163.com";
+	
+	
+	public static void main(String[] args) {
+
+		// 获取系统属性
+		Properties properties = System.getProperties();
+
+		// 设置邮件服务器
+		properties.setProperty("mail.smtp.host", MAIL_SMTP_HOST);
+		properties.put("mail.smtp.auth", MAIL_SMTP_AUTH);
+
+		// 获取默认session对象
+		Session session = Session.getDefaultInstance(properties, new Authenticator() {
+			public PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(MAIL_SENDER, MAIL_SENDER_PWD); // 发件人邮件用户名、密码
+			}
+		});
+
+		try {
+			// 创建默认的 MimeMessage 对象
+			MimeMessage message = new MimeMessage(session);
+
+			// Set From: 头部头字段
+			message.setFrom(new InternetAddress(MAIL_SENDER));
+
+			// Set To: 头部头字段
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(MAIL_RECEIVER));
+
+			// Set Subject: 头部头字段
+			message.setSubject("This is the Subject Line!");
+
+			// 设置消息体
+			message.setText("This is actual message");
+
+			// 发送消息
+			Transport.send(message);
+			System.out.println("Sent message successfully....");
+		} catch (MessagingException mex) {
+			mex.printStackTrace();
+		}
+	}
 }
